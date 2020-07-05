47c6d24711789ca2b6a08a173cbc564894504b57
James <jcs21@aber.ac.uk>
2020-07-05 14:58:15 +0100

    fixed CI issue by including serve package and altering package.json in /frontend (#2)

---
+++ frontend/build/precache-manifest.e5973b30d0e0ddbc7b83e93278aff68d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad177e9691d35ba5358e98c419eaf769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9be818da1c5c4cf9756b",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e9c825f2b3aaf59fd7e0",
+    "url": "/static/css/main.7baaa957.chunk.css"
+  },
+  {
+    "revision": "9be818da1c5c4cf9756b",
+    "url": "/static/js/2.26c45dbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.26c45dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c825f2b3aaf59fd7e0",
+    "url": "/static/js/main.8e023456.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "8f22b95cf6867466dab56e3b66125bc9",
+    "url": "/static/media/dashbored-icons-pattern.8f22b95c.png"
+  },
+  {
+    "revision": "2e663f96a08b91010c843d54a0ebba86",
+    "url": "/static/media/logo.2e663f96.png"
+  }
+]);
